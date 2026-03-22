74018f41596bfac354cfbb22a80222267b196d54
Popchis <benajmaster@gmail.com>
2026-03-22 03:05:33 -0300

    se agrego otro mensaje de prueba

--- src/main/scala/Yapper.scala
+++ src/main/scala/Yapper.scala
@@ -1,12 +1,14 @@
 object Yapper {
 
-  val greeting:String=""
-  val farewell:String=""
+  val greeting: String = ""
+  val farewell: String = ""
+
   def main(args: Array[String]): Unit = {
     println("Hello my fellow memeros")
+    println("i hate windows so much i dont know how")
   }
 
-  def say(something:String):Unit={
+  def say(something: String): Unit = {
 
   }
 }
